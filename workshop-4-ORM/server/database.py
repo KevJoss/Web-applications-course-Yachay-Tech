4ebcc282c69805613f4a207e1dfb72baf40af5d1
KevJoss <kelth.joss53@gmail.com>
2026-09-18 08:52:25 -0500

    feat: set up database connection and server configuration files

---
+++ workshop-4-ORM/server/database.py
@@ -0,0 +1,39 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+
+DATABASE_URL = (
+    "mysql+pymysql://ormuser:ormpass123@localhost:3306/webshop"
+)
+
+
+# Central point of communication between Python and MySQL Server
+# echo=True -> shows the SQL queries on the console
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True
+)
+
+# Session Maker is a factory which open Local Sessions for doing CRUD on the open engine
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+
+    db = SessionLocal() # Open new session
+
+    try:
+        yield db # Gives to the endpoint the session
+
+    finally:
+        db.close() # Close the session when ENDS de process.
